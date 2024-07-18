a15d24fe5645e0e139d13e448347af95be3ac0a0
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-07-18 12:55:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,8 +71,8 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://sititac651
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'VSAFBFILES') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/VenomStoneNetwork'')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/VenomStoneNetwork'')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/VenomStoneNetwork')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/VenomStoneNetwork')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/VenomStoneMovies/2503')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'VenomStoneNetwork'') # Support Chat Link Without https:// or @
 
